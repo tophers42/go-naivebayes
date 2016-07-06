a3e95543a5c592f84c8d086e1388a0027dc4e6d1
Christoph Neyer <cneyer@turnitin.com>
2016-07-06 10:47:19 -0700

    More doc updates

--- naivebayes/naivebayes.go
+++ naivebayes/naivebayes.go
@@ -1,3 +1,5 @@
+// Package naivebayes provides functionality and structs for building and training a naivebayes
+// text classifier  model and making predictions based on that model.
 package naivebayes
 
 /*
@@ -15,13 +17,15 @@ import (
 	"strings"
 )
 
-// Observation struct. Represents an instance of text to be classified (or for training).
+// Observation struct.
+// Represents an instance of text to be classified (or for training).
 type Observation struct {
 	Classes    []string
 	WordCounts map[string]int
 }
 
-// Creates an observation object. Breaks up a block of text on " " and calculates word counts.
+// NewObservationFromText creates an observation object.
+// Breaks up a block of text on " " and calculates word counts.
 func NewObservationFromText(classes []string, text string) *Observation {
 	counts := make(map[string]int)
 	for _, word := range strings.Split(text, " ") {
@@ -38,7 +42,8 @@ func NewObservationFromText(classes []string, text string) *Observation {
 // Returned when calling Predict on a trained model with a new observation.
 type Prediction map[string]float64
 
-// Filters a prediction down to the best fit, returning just the class name and calulated probability.
+// BestFit filters a prediction down to the best fit,
+// returning just the class name and calulated probability.
 func (p *Prediction) BestFit() (bestClassName string, bestProbability float64) {
 	for className, probability := range *p {
 		if bestClassName == "" || probability > bestProbability {
@@ -49,7 +54,8 @@ func (p *Prediction) BestFit() (bestClassName string, bestProbability float64) {
 	return bestClassName, bestProbability
 }
 
-// Class struct (a.k.a category). Represents a grouping of observations that belong together.
+// Class struct (a.k.a category).
+// Represents a grouping of observations that belong together.
 type Class struct {
 	Name             string
 	ObservationCount int
@@ -57,18 +63,19 @@ type Class struct {
 	TotalCount       int
 }
 
-// Creates an empty class struct
+// NewClasse creates an empty class struct.
 func NewClass(name string) *Class {
 	return &Class{Name: name, WordCounts: make(map[string]int), TotalCount: 0}
 }
 
-// Increments the count for the given word on the class.
+// addWord increments the count for the given word on the Class.
 func (c *Class) addWord(word string, count int) {
 	c.WordCounts[word] += count
 	c.TotalCount += count
 }
 
-// Model struct. Represents a training set and can be used to make class membership
+// Model struct.
+// Represents a training set and can be used to make class membership
 // predictions on new observations.
 type Model struct {
 	Name             string
@@ -77,13 +84,13 @@ type Model struct {
 	Vocabulary       map[string]int
 }
 
-// Creates and empty model with a name
+// NewModel creates and empty Model with the given name.
 func NewModel(name string) *Model {
 	return &Model{Name: name, Classes: make(map[string]*Class), ObservationCount: 0, Vocabulary: make(map[string]int)}
 }
 
-// Creates a new model and loads its state from the given file.
-// ( Model state is stored as a simple json dump of the Model struct)
+// NewModelFromFile creates a new Model and loads its state from the given file.
+// (Model state is stored as a simple json dump of the Model struct).
 func NewModelFromFile(path string) (m *Model, err error) {
 	// read the whole thing at once
 	m = &Model{}
@@ -98,8 +105,8 @@ func NewModelFromFile(path string) (m *Model, err error) {
 	return m, nil
 }
 
-// Dumps the current state of the Model to the given file.
-// ( Model state is stored as a simple json dump of the Model struct)
+// SaveToFile dumps the current state of the Model to the given file.
+// (Model state is stored as a simple json dump of the Model struct).
 func (m *Model) SaveToFile(path string) (err error) {
 	jsonModel, _ := json.Marshal(m)
 	// write the whole thing at once
@@ -107,7 +114,7 @@ func (m *Model) SaveToFile(path string) (err error) {
 	return err
 }
 
-// Trains the Model with the given Observation
+// Train updates (trains) the Model with the given Observation.
 func (m *Model) Train(o *Observation) {
 	for _, className := range o.Classes {
 		class, ok := m.Classes[className]
@@ -125,8 +132,8 @@ func (m *Model) Train(o *Observation) {
 	m.ObservationCount++
 }
 
-// Calculates the posterior probabality for the given observation for each
-// class within the Model. Returns a Prediction object.
+// Predict caalculates the posterior probabality for the given observation
+// for each class within the Model.
 func (m *Model) Predict(o *Observation) (p Prediction) {
 	p = make(map[string]float64)
 
@@ -137,10 +144,9 @@ func (m *Model) Predict(o *Observation) (p Prediction) {
 	return p
 }
 
-/*
-	Calculates the prior probability of the given class for the Model.
-	P( class ) - Number of occurences of the class / Number of trained observations
-*/
+// classPriorProbability calculates the prior probability of the given class
+// for the Model.
+// P( class ) - Number of occurences of the class / Number of trained observations
 func (m *Model) classPriorProbability(class *Class) (p float64) {
 	p = math.Log(float64(class.ObservationCount) / float64(m.ObservationCount))
 	return p
@@ -148,16 +154,18 @@ func (m *Model) classPriorProbability(class *Class) (p float64) {
 }
 
 /*
-	Calculates the conditional probability of the given observation for the
-	given class within the Model.
+	classConditionalProbability calculates the conditional probability of the
+	given observation for the given class within the Model.
 	P( observation | class ) - conditional probability of this observation
 	given the class.
-	Multiplies the probabilities of each word being in this class (sum logs)
+
+	Multiplies the probabilities of each word being in this class (sum logs).
 	P( word | class ) - probability of a word, given a class
 	Occurences of the word within the class, divided by total number of words
-	( words in class plus total number of unique words seen by the model )
+	(words in class plus total number of unique words seen by the model)
+
 	Laplace smoothing (always add one so new words don't break everything)
-	and also add unique word count to denominator
+	and also add unique word count to denominator.
 */
 func (m *Model) classConditionalProbability(class *Class, o *Observation) (p float64) {
 	p = 0
