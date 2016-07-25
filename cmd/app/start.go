e9a862348569532d0a7f7d15199863759f3759a8
Christoph Neyer <cneyer@turnitin.com>
2016-07-25 11:30:23 -0700

    comment out start.go for now

--- cmd/app/start.go
+++ cmd/app/start.go
@@ -1,16 +1,10 @@
 package main
 
-import (
-	"flag"
-
-	"github.com/tophers42/go-naivebayes/naivebayes"
-)
-
 // Create the application, register endpoints and start it.
 func main() {
-	configFile := flag.String("config_file", nil, "Config file path. Other options override config")
-	portPointer := flag.String("port", ":8080", "Port to take requests on format = ':<port_number>'")
-	flag.Parse()
+	// configFile := flag.String("config_file", "config.yml", "Config file path. Other options override config")
 
-	naivebayes.NewApp(*portPointer).StartServer()
+	// conf := &naivebayes.Config{}
+	// naivebayes.LoadFromFile(*configFile, &conf, yaml.Unmarshal)
+	// naivebayes.NewNaiveBayesApp(conf).StartServer()
 }
